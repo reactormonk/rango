84dd84121121c28943260e23f6085659385a12ce
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-10-24 19:18:37 +0100

    No avatars in Tweets list

--- javascripts/initializers/twitter-gitter.js
+++ javascripts/initializers/twitter-gitter.js
@@ -5,7 +5,8 @@ window.addEvent('domready', function() {
 		onComplete: function(tweets, user) {
 			tweets.each(function(tweet, i) {
 				new Element('div', {
-					html: '<img src="' + user.profile_image_url.replace("\\",'') + '" align="left" alt="' + user.name + '" /> <strong>' + user.name + '</strong><br />' + tweet.text + '<br /><span>' + tweet.created_at + ' via ' + tweet.source.replace("\\",'') + '</span>',
+					//html: '<img src="' + user.profile_image_url.replace("\\",'') + '" align="left" alt="' + user.name + '" /> <strong>' + user.name + '</strong><br />' + tweet.text + '<br /><span>' + tweet.created_at + ' via ' + tweet.source.replace("\\",'') + '</span>',
+					html: '<strong>' + user.name + '</strong><br />' + tweet.text + '<br /><span>' + tweet.created_at + '</span><br>',
 					'class': 'tweet clear'
 				}).inject('tweets');
 			});
