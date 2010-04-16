4d7c97dfe167c7ecef22cf1628ebb942f42dab38
Simon Hafner <hafnersimon@gmail.com>
2010-04-16 17:09:57 +0200

    added Controller#to_response tests

--- test/suite/lib/rango/controller.rb
+++ test/suite/lib/rango/controller.rb
@@ -53,5 +53,60 @@ BareTest.suite "Rango" do
       end
     end
 
+    suite "#to_response" do
+      setup :controller, "a silent controller" do
+        @block = proc do
+          def show
+            "make me a cucumber sandwitch"
+          end
+        end
+        @check = /cucumber/
+      end
+      setup :controller, "a controller raising a redirection" do
+        @block = proc do
+          def show
+            raise Rango::Exceptions::Redirection.new("/foo", 303)
+          end
+        end
+        @check = /Please follow/
+      end
+      setup :controller, "a controller raising an HttpError" do
+        @block = proc do
+          def show
+            raise Rango::Exceptions::InternalServerError
+          end
+        end
+        @check = /Internal Server Error/
+      end
+      setup :controller, "a controller raising an Exception in production" do
+        Rango.environment = "production"
+        @block = proc do
+          def show
+            raise "sudo make me a sandwitch"
+          end
+        end
+        @check = /Application Error/
+      end
+
+      setup :exercise do
+        Rango::Router.use(:usher)
+        @controller_class = Class.new(Rango::Controller, &@block)
+        @controller = @controller_class.new(@env)
+      end
+
+      assert ":controller returns a valid rack response" do
+        # I'm using call too here, doesn't matter.
+        Rack::Lint.new(@controller_class).call(@env)
+      end
+
+      assert "it handles :controller correctly" do
+        Array(@controller.to_response[2]).first =~ @check
+      end
+
+      teardown do
+        Rango.environment = "test"
+      end
+    end
+
   end
 end
